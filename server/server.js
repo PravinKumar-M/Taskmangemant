7cf5523520ad1e80aec541f28acd7576a54666af
pravin kumar M <pravinkumarm546@gmail.com>
2026-05-12 21:43:29 +0530

    3

--- server/server.js
+++ server/server.js
@@ -84,6 +84,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`=================================`);
   console.log(`Server running on port ${PORT}`);
-  console.log(`http://10.150.20.137:${PORT}`);
+  console.log(`http://naveen.hummingtone.com:${PORT}`);
   console.log(`=================================`);
 });
